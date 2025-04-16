bda44d06a9b427549e6c83e5d9c7cc04ff3b3b7f
allan-villneuve <allansassuk@gmail.com>
2025-04-16 10:36:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const botao = 
+document.getElementById("botaoClick");
+const mensagem = 
+document.getElementById("textoMensagem");
+
+botao.onclick = function()
+{mensagem.textContent = "Seja bem vindo a minha pagina pessoal!";
+ 
+botao.style.backgroundColor = "lightgreen";
+
+};
+
+
+
+
